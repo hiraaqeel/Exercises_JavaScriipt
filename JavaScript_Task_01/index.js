fa7ccbb27b80e2b0dc2c0f684da9ecd6076ff03f
hiraaqeel <hiraaqeel987@gmail.com>
2024-02-17 20:36:11 +0500

    Chapter-35:Functions Practice

---
+++ JavaScript_Task_01/index.js
@@ -0,0 +1,33 @@
+// WELCOME MESSAGE:
+// Create a function called displayWelcomeMessage that prompts the user to enter their name and then displays a personalized welcome message such as "Welcome, [name]!" in an alert.
+
+// function displayWelcomeMessage(){
+//     const userName = prompt("Enter your Name");
+//     alert("Welcome "+userName+"!");
+// }
+// displayWelcomeMessage();
+
+
+// QUOTE GENERATOR: 
+// Create a function called generateQuote that randomly selects and returns a quote from an array of predefined quotes. Call this function to display a random quote on the webpage.
+
+// function generateQuote(){
+//     const quotes = [
+//     "The only way to do great work is to love what you do. - Steve Jobs",
+//     "Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll",
+//     "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+//     "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+//     "The only impossible journey is the one you never begin. - Tony Robbins"];
+//     const randomIndex = Math.floor(Math.random() * quotes.length);
+//     console.log(quotes[randomIndex]);
+// }
+// generateQuote()
+
+
+// RANDOM NUMBER GENERATOR: 
+// Write a function called generateRandomNumber that generates and returns a random number between 1 and 100. Call this function and display the generated random number on the webpage.
+
+// function generateRandomNumber(){
+//     console.log(Math.floor(Math.random() * 100) + 1);
+// }
+// generateRandomNumber()

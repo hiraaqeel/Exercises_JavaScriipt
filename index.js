30c1fa988347ff1b450ceda81344fdf10d46ee0e
hiraaqeel <hiraaqeel987@gmail.com>
2024-02-17 20:01:54 +0500

    add JavaScript Exercises

---
+++ index.js
@@ -0,0 +1,84 @@
+// EXERCISE # 01
+// Sum of all digits of a number. For example, 123 -> 1+2+3=6
+
+// let number = 789;
+// let numberString = number.toString(); 
+// let sum = 0;
+
+// for (let i = 0; i < numberString.length; i++) {
+//     sum += parseInt(numberString[i]); 
+// }
+
+// console.log("Sum of digits:", sum);
+
+
+// EXERCISE # 02
+// Sum of the range of 2 numbers. For example, num1=2, num2=5 -> 2+3+4+5=14
+
+// const firstNum = +prompt("Enter First Number")
+// const secondNum = +prompt("Enter Second Number")
+// let sum = 0;
+// for(let i = firstNum; i<=secondNum; i++){
+//     sum += i 
+// }
+// console.log(sum)
+
+
+// EXERCISE # 03
+// Calculate the average of numbers given in an array.
+
+// var number = [2,5,4,7,8]
+// console.log(number)
+// var sum = 0;
+// for(let i = 0; i<number.length;i++){
+//     sum = sum + number[i]
+//     console.log(sum)
+// }
+// let average = sum / number.length;
+// console.log(average)
+
+
+// EXERCISE # 04
+// Compare strings by count of characters. For example, car and bat are equal. car and care are not.
+
+// const str1 = prompt("Enter any Name")
+// const str2 = prompt("Enter another Name")
+// if( str1.length === str2.length){
+//     console.log("The strings are equal")
+// }
+// else{
+//     console.log("The strings are not equal")
+// }
+
+
+// EXERCISE # 05
+// Swap the string's first and last character. For example, name -> eamn
+
+// let str = "NAME";
+// console.log("Original String:",str)
+
+// // Converting string to array of characters
+// let chars = [];
+// for (let i = 0; i < str.length; i++) {
+//     chars.push(str[i]);
+//     // console.log(chars)
+// }
+
+// // Swap first and last elements
+// let temp = chars[0];
+// chars[0] = chars[chars.length - 1];
+// chars[chars.length - 1] = temp;
+// // console.log(temp,chars[0])
+
+// // Join array back into a string
+// let swappedStr = "";
+// for (let i = 0; i < chars.length; i++) {
+//     swappedStr += chars[i];
+//     // console.log(swappedStr)
+// }
+
+// console.log("Swapped string:", swappedStr);
+
+
+
+    
